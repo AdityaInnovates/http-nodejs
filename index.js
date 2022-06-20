21229f4d4ced64e3941f07c915563a66ff3c36f2
Aditya Kumar <adityakr80510@gmail.com>
2022-06-20 15:19:50 +0530

    make it better

--- index.js
+++ index.js
@@ -19,19 +19,19 @@ const { Blob } = require("buffer");
 const { URLSearchParams } = require("url");
 const extractFrames = require("ffmpeg-extract-frames");
 
-// const client = new Client();
+const client = new Client();
 
-const client = new Client({
-  puppeteer: {
-    executablePath: "/usr/bin/brave-browser-stable",
-  },
-  authStrategy: new LocalAuth({
-    clientId: "client-one",
-  }),
-  puppeteer: {
-    headless: true,
-  },
-});
+// const client = new Client({
+//   puppeteer: {
+//     executablePath: "/usr/bin/brave-browser-stable",
+//   },
+//   authStrategy: new LocalAuth({
+//     clientId: "client-one",
+//   }),
+//   puppeteer: {
+//     headless: false,
+//   },
+// });
 
 client.on("authenticated", (session) => {
   console.log("WHATSAPP WEB => Authenticated");
