2d955fe3a5289e8242e6f8581d57b153eb2d576d
Aditya Kumar <adityakr80510@gmail.com>
2022-06-20 16:31:59 +0530

    better

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const {
   LegacySessionAuth,
   LocalAuth,
   MessageMedia,
+  RemoteAuth,
 } = require("whatsapp-web.js");
 const qrcode = require("qrcode-terminal");
 const fs = require("fs");
@@ -21,7 +22,6 @@ const { URLSearchParams } = require("url");
 var express = require("express");
 var app = express();
 var path = require("path");
-const { Client, RemoteAuth } = require("whatsapp-web.js");
 const { MongoStore } = require("wwebjs-mongo");
 const mongoose = require("mongoose");
 
